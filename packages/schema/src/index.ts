4ccc794afaca19eef290fe22c372f3fb83bce11d
RedCode64 <203717332+RedCode64@users.noreply.github.com>
2026-07-16 11:45:13 -0400

    feat(schema): expose public schema surface

--- packages/schema/src/index.ts
+++ packages/schema/src/index.ts
@@ -1,1 +1,67 @@
-export {};
+export {
+  centsField,
+  dateField,
+  markupBpsField,
+  positiveCentsField,
+  quantityField,
+  rateBpsField,
+  signedCentsField,
+  syncColumns,
+  timestampField,
+  uuidField,
+} from "./common.js";
+export {
+  estimateStatusSchema,
+  invoiceStatusSchema,
+  jobStatusSchema,
+  lineKindSchema,
+  membershipRoleSchema,
+  paymentMethodSchema,
+  priceBookKindSchema,
+  tradeSchema,
+  type EstimateStatus,
+  type InvoiceStatus,
+  type JobStatus,
+  type LineKind,
+  type MembershipRole,
+  type PaymentMethod,
+  type PriceBookKind,
+  type Trade,
+} from "./enums.js";
+export {
+  membershipSchema,
+  organizationSchema,
+  taxConfigSchema,
+  userSchema,
+  type Membership,
+  type Organization,
+  type TaxConfig,
+  type User,
+} from "./org.js";
+export { clientSchema, type Client } from "./client.js";
+export { jobSchema, type Job } from "./job.js";
+export { priceBookItemSchema, type PriceBookItem } from "./priceBook.js";
+export { documentLineFields } from "./lines.js";
+export {
+  estimateLineSchema,
+  estimateSchema,
+  type Estimate,
+  type EstimateLine,
+} from "./estimate.js";
+export {
+  invoiceLineSchema,
+  invoiceSchema,
+  paymentSchema,
+  type Invoice,
+  type InvoiceLine,
+  type Payment,
+} from "./invoice.js";
+export {
+  budgetSchema,
+  expenseCategorySchema,
+  expenseSchema,
+  type Budget,
+  type Expense,
+  type ExpenseCategory,
+} from "./expense.js";
+export { photoSchema, signatureSchema, type Photo, type Signature } from "./media.js";
